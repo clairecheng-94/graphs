411a3cbe7a86b87d9b0eedf34f99a193757aee7c
clairecheng-94 <76176353+clairecheng-94@users.noreply.github.com>
2023-03-24 14:13:43 -0400

    Update heatmap.r

--- heatmap.r
+++ heatmap.r
@@ -6,4 +6,7 @@ library("reshape2")
 
 ## READ THE TABLE INTO THE.
 for_tab<-read.table('/scratch/cfc85413/PUFAS/data/forward.table.tsv',  header=TRUE, sep= "\t")
+exposure<-for_tab[,c(1,2,11)]
+df<-acast(exposure,outcome_name~exposure_name)
+
 
