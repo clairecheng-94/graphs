522828e9a58864709fc90dddf45d33b07315e039
clairecheng-94 <76176353+clairecheng-94@users.noreply.github.com>
2023-03-23 20:33:32 -0400

    Update heatmap.r

--- heatmap.r
+++ heatmap.r
@@ -1,3 +1,9 @@
+interact -c 4 --mem 50G
+cd /scratch/cfc85413/PUFAS/data
+ml R/4.2.1-foss-2020b
+
+library("reshape2")
+
 ## READ THE TABLE INTO THE.
 for_tab<-read.table('/scratch/cfc85413/PUFAS/data/forward.table.tsv',  header=TRUE, sep= "\t")
 
